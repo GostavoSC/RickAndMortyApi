28bbdf0bb3a8305d68241b95b4145e0bedeed00a
gustavo-stelle-cunha <gustavo.cunha@hbsis.com.br>
2020-11-04 14:53:47 -0300

    -Adicionando observer do viewmodel

--- app/src/main/java/com/gustavosc/rickandmortyapi/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/gustavosc/rickandmortyapi/ui/main/view/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.gustavosc.rickandmortyapi.ui.main.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import com.gustavosc.rickandmortyapi.R
+import com.gustavosc.rickandmortyapi.data.api.ApiHelper
+import com.gustavosc.rickandmortyapi.data.api.RetrofitBuilder
+import com.gustavosc.rickandmortyapi.ui.base.MainViewModelFactory
+import com.gustavosc.rickandmortyapi.ui.main.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViewModel()
+        setupObseverListOfCharacter()
+    }
+
+    private fun setupObseverListOfCharacter() {
+        viewModel.getAllCharacters().observe(this){
+            it
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModel = ViewModelProviders.of(
+            this,
+            MainViewModelFactory(ApiHelper((RetrofitBuilder.apiService)))
+        ).get(MainViewModel::class.java)
     }
 }
--- app/src/main/java/com/gustavosc/rickandmortyapi/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/gustavosc/rickandmortyapi/ui/main/viewmodel/MainViewModel.kt
@@ -15,7 +15,6 @@ class MainViewModel(private val mainRepository: MainRepository): ViewModel() {
             }
         }
 
-
     }
 
 }
